bd4ec96b4d81dacf65c135290b84f44f80652087
Hanka <hrosicka@post.cz>
2025-05-20 17:03:54 +0200

    preliminary dark theme implemented

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const darkModeSwitch = document.getElementById('darkModeSwitch');
 
-// Write your JavaScript code.
+function setTheme(theme) {
+    document.body.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme); // Uložení preferovaného tématu do localStorage
+}
+
+if (darkModeSwitch) { // Kontrola, zda prvek existuje na stránce
+    darkModeSwitch.addEventListener('change', () => {
+        if (darkModeSwitch.checked) {
+            setTheme('dark');
+        } else {
+            setTheme(null); // Nebo 'light', pokud chceš explicitní světlý režim
+        }
+    });
+
+    // Při načtení stránky zkontroluj uložené téma
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        setTheme('dark');
+        darkModeSwitch.checked = true;
+    } else if (savedTheme) { // Pokud je uložené jiné téma (např. 'light')
+        setTheme(savedTheme);
+        darkModeSwitch.checked = (savedTheme === 'dark');
+    }
+}
